52d211186a053376867b68f186ecfd5a224b65ff
pavithira060507 <pavithira060507@gmail.com>
2026-06-08 09:04:03 +0530

    Added follow feature and multiple posts

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let likes = 0;
+let followers = 0;
 
 function likePost(){
     likes++;
     alert("Post liked! Total Likes: " + likes);
+}
+
+function followUser(){
+    followers++;
+    alert("You are now following this user!");
 }
